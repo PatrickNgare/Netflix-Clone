4d769cd2ac4b563f5f4fb77fc7452d4fc12a8908
patrickNgare <patrickngare@gmail.com>
2018-02-28 19:21:08 +0300

    added index views

--- netflix/netflify/views.py
+++ netflix/netflify/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+import tmdbsimple as tmdb
 
+
+tmdb.API_KEY='9c82e3db6480e76674d7cd0897cfd06b'
+ 
+def index(request):
+    
+   
+    populars = tmdb.Movies().popular()['results']
+    
+    print(populars)
+    
+   
+
+    return render(request,'temps/index.html',{"populars": populars})
 
